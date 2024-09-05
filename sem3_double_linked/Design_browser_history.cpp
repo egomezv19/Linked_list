683d8602c75db2444233a9f10cb1c25d7288c737
egomezv19 <enzoo.g.17@gmail.com>
2024-09-04 23:23:36 -0500

    sem 3

---
+++ sem3_double_linked/Design_browser_history.cpp
@@ -0,0 +1,43 @@
+class BrowserHistory {
+public:
+stack<string> stack1;
+stack<string> stack2;
+
+    BrowserHistory(string homepage) {
+        stack.push(homepage)
+    }
+    
+    void visit(string url) {
+        stack1.push(url);
+        while(!stack2.empty()){
+            stack2.pop();
+        }
+    }
+    
+    string back(int steps) {
+        while(stack1.size()!=1 && steps>0){
+            stack2.push(stack1.top());
+            stack1.pop();
+            steps--;
+        }
+        return stack1.top;
+    }
+    
+    string forward(int steps) {
+        while (stack2.empty()==false && steps>0){
+            stack1.push(stack2.top())
+            stack2.pop());
+            steps--;
+        
+        }
+        return stack1.top();
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
